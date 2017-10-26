a4af0609ecad0157396a77be4d45699aed0bb754
SrOverLord <30776151+SrOverLord@users.noreply.github.com>
2017-10-25 22:59:47 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This pair of functions are used to create a special object that stores a numeric vector and cache's its mean
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# The first function is used to create a vector with four parameters
+# set and get of the vector and the mean
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
+# The second function calculates the average of the vector,
+# verifies if the average has already been calculated
+# if it is obtained the average of the cache
+# if it does not calculate the average of the data and the average in the cache memory.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
+
+#Checking the program
+#We enter in the first function a vector
+#with values from 1 to 5 and save it in a variable x
+# x <- makeVector(c(1,2,3,4,5))
+#and finally we enter the value of x to the second function
+# cachemean(x)
+#the result will be the average of our vector
+#[1] 3
+#
+
+
+
